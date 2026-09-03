28dc1aadae6b82c1376e2137ef4523c487ea7f17
Mateusz Dropiński <mateuszdropinski5@gmail.com>
2026-09-03 11:26:10 +0200

    Use compact league emblems that remain readable in navigation

--- src/data/leagues.ts
+++ src/data/leagues.ts
@@ -1,10 +1,10 @@
 export const leagues = [
   { slug: 'premier-league', logo: 'https://www.premierleague.com/resources/v1.52.5/i/favicon/favicon-96x96.png', logoOnLight: true, name: 'Premier League', country: 'England', teamCount: 20 },
   { slug: 'la-liga', logo: 'https://upload.wikimedia.org/wikipedia/commons/2/20/LaLiga_2023_Vertical_Logo.svg', logoOnLight: false, name: 'La Liga', country: 'Spain', teamCount: 20 },
-  { slug: 'bundesliga', logo: 'https://www.bundesliga.com/assets/logo/bundesliga_neg.svg', logoOnLight: false, name: 'Bundesliga', country: 'Germany', teamCount: 18 },
+  { slug: 'bundesliga', logo: 'https://www.bundesliga.com/assets/favicons/favicon-32x32.png', logoOnLight: false, name: 'Bundesliga', country: 'Germany', teamCount: 18 },
   { slug: 'serie-a', logo: 'https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/53b06b9b-9fef-462d-ae9f-3575cbf6eac4/Logo_Serie-A_2025.png', logoOnLight: true, name: 'Serie A', country: 'Italy', teamCount: 20 },
-  { slug: 'ligue-1', logo: 'https://ligue1.com/images/Logo_Ligue1.webp', logoOnLight: false, name: 'Ligue 1', country: 'France', teamCount: 18 },
-  { slug: 'ekstraklasa', logo: 'https://media.cms.ekstraklasa.org/images/originals/019f1d29-4986-7439-8e66-50bc7019bd1b.png?height=40&format=webp', logoOnLight: false, name: 'Ekstraklasa', country: 'Poland', teamCount: 18 },
+  { slug: 'ligue-1', logo: 'https://ligue1.com/favicon.ico', logoOnLight: false, name: 'Ligue 1', country: 'France', teamCount: 18 },
+  { slug: 'ekstraklasa', logo: 'https://ekstraklasa.org/icon.png?icon.0wu37xahwa004.png', logoOnLight: false, name: 'Ekstraklasa', country: 'Poland', teamCount: 18 },
 ] as const
 
 export type LeagueSlug = typeof leagues[number]['slug']
